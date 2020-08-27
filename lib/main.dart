73ae6df5312e47e23a23a73f7a47c300fb7b8949
Susan Adesoji <adesojisusan.adeola@gmail.com>
2020-08-27 16:29:13 +0100

    Initial Commits

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
-
-void main() => runApp(XylophoneApp());
-
-class XylophoneApp extends StatelessWidget {
-  void playSound(int soundNumber) {
+void main (){
+  runApp(xylophone()
+  );
+}
+ 
+class xylophone extends StatelessWidget {
+  void produceSound (int play){
     final player = AudioCache();
-    player.play('note$soundNumber.wav');
+    player.play('note$play.wav');
   }
-
-  Expanded buildKey({Color color, int soundNumber}) {
+  Expanded buildKey ({Color colors, int play})
+  {
     return Expanded(
-      child: FlatButton(
-        color: color,
-        onPressed: () {
-          playSound(soundNumber);
-        },
+      child: FlatButton
+        ( color:color,
+        onPressed:(){
+        produceSound(play);
+      },
       ),
     );
   }
@@ -26,18 +28,17 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                buildKey(color: Colors.red, soundNumber: 1),
-                buildKey(color: Colors.orange, soundNumber: 2),
-                buildKey(color: Colors.yellow, soundNumber: 3),
-                buildKey(color: Colors.green, soundNumber: 4),
-                buildKey(color: Colors.teal, soundNumber: 5),
-                buildKey(color: Colors.blue, soundNumber: 6),
-                buildKey(color: Colors.purple, soundNumber: 7),
-              ],
-          ),
+       child:Column(
+         crossAxisAlignment: CrossAxisAlignment.stretch,
+         children:[
+          buildKey(color:Colors.red, play: 1 ),
+          buildKey(),
+          buildKey(),
+          buildKey(),
+          buildKey(),
+          buildKey(),
+         ]
+         ),
         ),
       ),
     );
